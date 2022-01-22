a5727149ba99282088fc1e4d76d0600fe81643a9
imakabr <imakabr@gmail.com>
2022-01-22 14:18:41 +0300

    fix RandomJVM

--- src/main/java/jvm/engine/ExecutionEngine.java
+++ src/main/java/jvm/engine/ExecutionEngine.java
@@ -36,7 +36,6 @@ public final class ExecutionEngine {
     private final static String INIT_PRINT_WRITER = "java/io/PrintWriter.initPrintWriter:(Ljava/io/OutputStream;Z)V";
     private final static String PRINT_WRITER_PRINTLN = "java/io/PrintWriter.println:(Ljava/lang/String;)V";
     private final static String READ_LINE = "java/io/BufferedReader.readLine:()Ljava/lang/String;";
-    private final static String INIT_RANDOM = "java/util/Random.initRandom:()V";
     private final static String RANDOM_NEXT_INT = "java/util/Random.nextInt:(I)I";
 
     private final Opcode[] table = new Opcode[256];
@@ -819,14 +818,10 @@ public final class ExecutionEngine {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        } else if (INIT_RANDOM.equals(methodName)) {
-            int randomObjRef = getPureValue(checkValueType(stack.pop(), JVMType.A, stackMethod, pointer, opcode));
-            nativeObjects.put(randomObjRef, new Random());
         } else if (RANDOM_NEXT_INT.equals(methodName)) {
             int bound = getPureValue(checkValueType(stack.pop(), JVMType.I, stackMethod, pointer, opcode));
             int randomObjRef = getPureValue(checkValueType(stack.pop(), JVMType.A, stackMethod, pointer, opcode));
-            Random random = (Random) nativeObjects.get(randomObjRef);
-            stack.push(setIntValueType(random.nextInt(bound)));
+            stack.push(setIntValueType(new Random().nextInt(bound)));
 
         }
     }
--- src/main/java/jvm/util/RandomJVM.java
+++ src/main/java/jvm/util/RandomJVM.java
@@ -3,10 +3,7 @@ package jvm.util;
 public class RandomJVM {
 
     public RandomJVM() {
-        initRandomJVM();
     }
-
-    private native void initRandomJVM();
 
     public native int nextInt(int bound);
 }
