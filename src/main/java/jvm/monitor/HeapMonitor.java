9d1782326fefb65d76b7f0e2d6db3382676c697f
imakabr <imakabr@gmail.com>
2022-05-27 12:06:38 +0300

    add class name groups to heap monitor

--- src/main/java/jvm/monitor/HeapMonitor.java
+++ src/main/java/jvm/monitor/HeapMonitor.java
@@ -15,17 +15,41 @@ import java.util.concurrent.BlockingQueue;
 
 public class HeapMonitor {
 
+    public static final String WITHOUT_GROUP = "withoutGroup";
+
     @Nonnull
     private final Heap heap;
     @Nonnull
-    private final Set<String> classNames;
+    private final Set<String> classNames = new HashSet<>();
+    @Nonnull
+    private final Map<String, Set<String>> groups = new HashMap<>();
     @Nonnull
     public static final BlockingQueue<Message> queue = new ArrayBlockingQueue<>(1);
 
 
     public HeapMonitor(@Nonnull Heap heap, @Nonnull Set<String> classNames) {
         this.heap = heap;
-        this.classNames = classNames;
+        parseClassNames(classNames);
+    }
+
+    private void parseClassNames(@Nonnull Set<String> strings) {
+        Set<String> withoutGroup = new HashSet<>();
+        for (String str : strings) {
+            if (str.contains("{")) {
+                Set<String> classNameSet = new HashSet<>();
+                String[] names = str.substring(str.indexOf("{") + 1, str.indexOf("}")).split(",");
+                for (String name : names) {
+                    classNameSet.add(name.trim());
+                }
+                String groupName = str.substring(0, str.indexOf("{")).trim();
+                groups.put(groupName, classNameSet);
+                classNames.addAll(classNameSet);
+            } else {
+                withoutGroup.add(str);
+            }
+        }
+        groups.put(WITHOUT_GROUP, withoutGroup);
+        classNames.addAll(withoutGroup);
     }
 
     public void run() {
@@ -81,12 +105,25 @@ public class HeapMonitor {
 
                 List<String> names = new ArrayList<>();
                 List<Integer> data = new ArrayList<>();
-                for (String name : classNames) {
-                    Integer klassIndex = instanceKlasses.get(name);
-                    if (klassIndex != null) {
-                        data.add(objectCounts.get(klassIndex));
-                        names.add(name);
-
+                for (Map.Entry<String, Set<String>> entry : groups.entrySet()) {
+                    if (entry.getKey().equals(WITHOUT_GROUP)) {
+                        for (String name : entry.getValue()) {
+                            Integer klassIndex = instanceKlasses.get(name);
+                            if (klassIndex != null) {
+                                names.add(name);
+                                data.add(objectCounts.get(klassIndex));
+                            }
+                        }
+                    } else {
+                        int count = 0;
+                        for (String name : entry.getValue()) {
+                            Integer klassIndex = instanceKlasses.get(name);
+                            if (klassIndex != null) {
+                                count += objectCounts.get(klassIndex);
+                            }
+                        }
+                        names.add(entry.getKey());
+                        data.add(count);
                     }
                 }
                 names.add("Other objects");
--- src/test/java/jvm/ShapesTest.java
+++ src/test/java/jvm/ShapesTest.java
@@ -26,7 +26,14 @@ public class ShapesTest {
         VirtualMachine virtualMachine = new VirtualMachine(10000, 50, 10000);
         virtualMachine.getKlassLoader().loadKlass(klass);
         Heap heap = virtualMachine.getHeap();
-        virtualMachine.runHeapMonitor(new HashSet<>(Arrays.asList("java/lang/String", "java/lang/StringBuilder")));
+        virtualMachine.runHeapMonitor(new HashSet<>(Arrays.asList("java/lang/String", "java/lang/StringBuilder",
+                "Shapes { jvm/examples/heap_test/Circle, " +
+                        "jvm/examples/heap_test/Cross, " +
+                        "jvm/examples/heap_test/Diamond, " +
+                        "jvm/examples/heap_test/Pentagon, " +
+                        "jvm/examples/heap_test/Square, " +
+                        "jvm/examples/heap_test/Star, " +
+                        "jvm/examples/heap_test/Triangle }")));
 
         int methodIndex = heap.getMethodRepo().getIndexByName(klass + "." + methodName);
         Method method = heap.getMethodRepo().getMethod(methodIndex);
